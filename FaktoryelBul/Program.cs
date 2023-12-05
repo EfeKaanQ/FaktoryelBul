58f9d86dbfa236a304eec7f1823bf905ea9e59da
efe33 <efe33@BPC04>
2023-12-05 11:04:51 +0300

    Proje dosyası ekle.

---
+++ FaktoryelBul/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaktoryelBul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fakbul s=new Fakbul();
+            Console.WriteLine("FAK BULUNACAK SAYIYI GİRİNİZ = ");
+            int sayi = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Sonuç=" + s.fak(sayi));
+        }
+
+    }
+    class Fakbul
+    {
+        public int fak(int sayi)
+        {
+            int hesapla = 1;
+            for (int i = 1; i <= sayi; i++)
+            {
+                hesapla = hesapla * i;
+            }
+            return hesapla;
+        }
+        
+       
+    }
+}
